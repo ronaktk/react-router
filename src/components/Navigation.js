798f382ac04cd23528429e39c642e52010f020fb
Ronak Patel <ronaktk@gmail.com>
2018-08-01 18:06:20 -0400

    initial commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return(
+        <div>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/About">About</NavLink>
+            <NavLink to="/Contact">Contact</NavLink>
+        </div>
+    );
+}
+
+export default Navigation;
